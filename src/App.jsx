0128cf669dd82bc5b5149e231f86cf5bd60c4a9a
amjadsh92 <amjadsharafeddien@gmail.com>
2025-04-06 14:50:55 +0300

    Design the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp, faArrowDown, faPlay, faPause, faArrowRotateRight } from "@fortawesome/free-solid-svg-icons";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <div id="app" className="app">
+    
+  <div id="title" className="title">
+   25 + 5 Clock
+  
+  </div>
+  <div id="break-session-length" className="break-session-length">
+      <div id="break-length" className="break-length">
+        <div>Break Length</div>
+        <div className="break-increment">
+        <FontAwesomeIcon icon={faArrowUp} className="arrow" />
+        <span>5</span>
+        <FontAwesomeIcon icon={faArrowDown} className="arrow" />
+        </div>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="session-length" className="session-length" >
+      <div>Session Length</div>
+      <div className = "session-increment">
+      <FontAwesomeIcon icon={faArrowUp} className="arrow" />
+      <span>25</span>
+      <FontAwesomeIcon icon={faArrowDown} className="arrow" />
+      
+      
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      </div>
+  </div>
+  <div className="session">
+    <div className="session-title">Session</div>
+    <div className="session-time">25:00</div>
+ </div>
+ <div className="controller">
+     <FontAwesomeIcon icon={faPlay} className="play" />
+     <FontAwesomeIcon icon={faPause} className="pause" />
+     <FontAwesomeIcon icon={faArrowRotateRight} className ="reset" />
+
+ </div>
+  </div>
   )
+      
 }
 
 export default App
