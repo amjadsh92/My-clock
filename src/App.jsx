7abbd1d19916eda8752cc2b395e7b6aac59f9f6c
amjadsh92 <amjadsharafeddien@gmail.com>
2025-04-06 15:49:53 +0300

    Transform the code into components

--- src/App.jsx
+++ src/App.jsx
@@ -16,41 +16,65 @@ function App() {
    25 + 5 Clock
   
   </div>
-  <div id="break-session-length" className="break-session-length">
-      <div id="break-length" className="break-length">
-        <div>Break Length</div>
-        <div className="break-increment">
-        <FontAwesomeIcon icon={faArrowUp} className="arrow" />
-        <span>5</span>
-        <FontAwesomeIcon icon={faArrowDown} className="arrow" />
-        </div>
-        
-      </div>
-      <div id="session-length" className="session-length" >
-      <div>Session Length</div>
-      <div className = "session-increment">
-      <FontAwesomeIcon icon={faArrowUp} className="arrow" />
-      <span>25</span>
-      <FontAwesomeIcon icon={faArrowDown} className="arrow" />
-      
-      
-      </div>
-
-      </div>
+  <BreakAndSessionLengths />
+  <Session />
+  <Controller />
   </div>
-  <div className="session">
+  )
+      
+}
+
+
+function BreakAndSessionLengths(){
+
+  
+  return(
+    <div id="break-session-length" className="break-session-length">
+    <div id="break-length" className="break-length">
+      <div>Break Length</div>
+      <div className="break-increment">
+      <FontAwesomeIcon icon={faArrowUp} className="arrow" />
+      <span>5</span>
+      <FontAwesomeIcon icon={faArrowDown} className="arrow" />
+      </div>
+      
+    </div>
+    <div id="session-length" className="session-length" >
+    <div>Session Length</div>
+    <div className = "session-increment">
+    <FontAwesomeIcon icon={faArrowUp} className="arrow" />
+    <span>25</span>
+    <FontAwesomeIcon icon={faArrowDown} className="arrow" />
+    
+    
+    </div>
+
+    </div>
+  </div>
+  )
+}
+
+
+function Session(){
+
+  return(
+    <div className="session">
     <div className="session-title">Session</div>
     <div className="session-time">25:00</div>
- </div>
- <div className="controller">
+    </div>    
+  )
+}
+
+function Controller(){
+
+  return(
+    <div className="controller">
      <FontAwesomeIcon icon={faPlay} className="play" />
      <FontAwesomeIcon icon={faPause} className="pause" />
      <FontAwesomeIcon icon={faArrowRotateRight} className ="reset" />
 
- </div>
-  </div>
+    </div>
   )
-      
 }
 
 export default App
