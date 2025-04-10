c3e5ac5bd42f1252f65e7222e1bd90858c45cbae
amjadsh92 <amjadsharafeddien@gmail.com>
2025-04-10 10:32:14 +0300

    Apply effect on arrows when clicking using pseudo class active

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
 
 
 function BreakAndSessionLengths({length, setLength, onPlay, setOnChangeSession, setOnChangeBreak}){
-  const [clicked, setClicked] = useState({on:false, type:""})
+  
   let {breakLength, sessionLength} = length
 
   const incrementBreak = () => {
@@ -51,8 +51,8 @@ function BreakAndSessionLengths({length, setLength, onPlay, setOnChangeSession, 
       breakLength += 1
       setLength({...length ,breakLength})
       setOnChangeBreak(true)
-      setClicked({on:true, type:"increment-break"})
-      setTimeout(()=> setClicked({on:false, type:""}) , 100)
+      
+      
 
     }
     
@@ -65,8 +65,7 @@ function BreakAndSessionLengths({length, setLength, onPlay, setOnChangeSession, 
     breakLength -= 1
     setLength({...length ,breakLength})
     setOnChangeBreak(true)
-    setClicked({on:true, type:"decrement-break"})
-    setTimeout(()=> setClicked({on:false, type:""}) , 100)
+    
     }
 
   }
@@ -77,8 +76,7 @@ function BreakAndSessionLengths({length, setLength, onPlay, setOnChangeSession, 
     sessionLength += 1
     setLength({...length ,sessionLength})
     setOnChangeSession(true)
-    setClicked({on:true, type:"increment-session"})
-    setTimeout(()=> setClicked({on:false, type:""}) , 100)
+    
     }
 
   }
@@ -89,8 +87,7 @@ function BreakAndSessionLengths({length, setLength, onPlay, setOnChangeSession, 
     sessionLength -= 1
     setLength({...length ,sessionLength})
     setOnChangeSession(true)
-    setClicked({on:true, type:"decrement-session"})
-    setTimeout(()=> setClicked({on:false, type:""}) , 100)
+    
     }
 
   }
@@ -101,18 +98,18 @@ function BreakAndSessionLengths({length, setLength, onPlay, setOnChangeSession, 
     <div id="break-length" className="break-length">
       <div>Break Length</div>
       <div className="break-increment">
-      <FontAwesomeIcon type = "submit" icon={faArrowUp} className={`arrow-up ${clicked.on && clicked.type === "increment-break" ? "clicked" : ""}`}  onClick={!onPlay ? incrementBreak: null}/>
+      <FontAwesomeIcon type = "submit" icon={faArrowUp} className={`arrow-up`}  onClick={!onPlay ? incrementBreak: null}/>
       <p>{breakLength}</p>
-      <FontAwesomeIcon type = "submit"  icon={faArrowDown} className={`arrow-down ${clicked.on && clicked.type === "decrement-break" ? "clicked" : ""}`} onClick={!onPlay ? decrementBreak: null} />
+      <FontAwesomeIcon type = "submit"  icon={faArrowDown} className={`arrow-down` } onClick={!onPlay ? decrementBreak: null} />
       </div>
       
     </div>
     <div id="session-length" className="session-length" >
     <div>Session Length</div>
     <div className = "session-increment">
-    <FontAwesomeIcon type = "submit"  icon={faArrowUp} className={`arrow-up ${clicked.on && clicked.type === "increment-session" ? "clicked" : ""}`} onClick={!onPlay ? incrementSession : null} />
+    <FontAwesomeIcon type = "submit"  icon={faArrowUp} className={`arrow-up`} onClick={!onPlay ? incrementSession : null} />
     <p>{sessionLength}</p>
-    <FontAwesomeIcon type = "submit"  icon={faArrowDown} className={`arrow-down ${clicked.on && clicked.type === "decrement-session" ? "clicked" : ""}`} onClick={!onPlay ? decrementSession: null} />
+    <FontAwesomeIcon type = "submit"  icon={faArrowDown} className={`arrow-down`} onClick={!onPlay ? decrementSession: null} />
     
     
     </div>
